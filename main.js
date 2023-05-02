1fdd16ff58f5965a13a0392b65676489802ed789
tokumei0000 <tokumei0090@gmail.com>
2023-05-02 18:20:00 +0900

    匿名です。

---
+++ main.js
@@ -0,0 +1,49 @@
+const { Player } = TextAliveApp;
+
+// 単語が発声されていたら #text に表示する
+const animateWord = function (now, unit) {
+  if (unit.contains(now)) {
+    document.querySelector("#text").textContent = unit.text;
+  }
+};
+
+// TextAlive Player を作る
+const player = new Player({
+  app: { token: "xxxxxxxxx" },
+});
+
+document.querySelector("#play").addEventListener("click", () => {
+  player.requestPlay();
+});
+document.querySelector("#play").disabled = true;
+
+
+player.addListener({
+  onTimerReady: () => {
+    document.querySelector("#play").disabled = false;
+  },
+  // 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
+  onVideoReady: (v) => {
+    // 定期的に呼ばれる各単語の "animate" 関数をセットする
+    let w = player.video.firstWord;
+    while (w) {
+      w.animate = animateWord;
+      w = w.next;
+    }
+  },
+  onAppReady: (app) => {
+    if (!app.managed) {
+      player.createFromSongUrl("https://piapro.jp/t/ucgN/20230110005414", {
+        video: {
+          // 音楽地図訂正履歴: https://songle.jp/songs/2427948/history
+          beatId: 4267297,
+          chordId: 2405019,
+          repetitiveSegmentId: 2405019,
+          // 歌詞タイミング訂正履歴: https://textalive.jp/lyrics/piapro.jp%2Ft%2FucgN%2F20230110005414
+          lyricId: 56092,
+          lyricDiffId: 9636
+        },
+      });
+    }
+  }
+});
